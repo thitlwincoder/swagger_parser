b7cdbd6bc46db9f22a5209d0e9334e341ce6b2b9
Thit Lwin <thitlwincoder@gmail.com>
2025-01-03 11:07:24 +0630

    feat: add support for class mock

--- swagger_parser/lib/src/generator/templates/dart_retrofit_client_mock_template.dart
+++ swagger_parser/lib/src/generator/templates/dart_retrofit_client_mock_template.dart
@@ -28,36 +28,36 @@ String dartRetrofitClientMockTemplate({
   final imports =
       getImports(restClient.imports, putInFolder, isMerge, '../').split('\n');
 
+  final importStr = StringBuffer('${_convertImport(restClient)}'
+      '${_fileImport(restClient)}'
+      "import 'package:faker/faker.dart';"
+      "${imports.join('\n')}"
+      "import '${name.toSnake.replaceFirst('_mock', '')}.dart';");
+
   final sb = StringBuffer(
     '''
-${generatedFileComment(markFileAsGenerated: markFileAsGenerated)}${_convertImport(restClient)}${_fileImport(
-      restClient,
-    )}import 'package:faker/faker.dart'${_hideHeaders(restClient, defaultContentType)};
-${imports.join('\n')}
-import '${name.toSnake.replaceFirst('_mock', '')}.dart';
-
 class $name implements ${name.replaceFirst('Mock', '')}{
 ''',
   );
   for (final request in restClient.requests) {
-    sb.write(
-      _toClientRequest(
-        request,
-        defaultContentType,
-        imports: imports,
-        mockData: mockData,
-        putInFolder: putInFolder,
-        originalHttpResponse: originalHttpResponse,
-        extrasParameterByDefault: extrasParameterByDefault,
-        dioOptionsParameterByDefault: dioOptionsParameterByDefault,
-      ),
+    final data = _toClientRequest(
+      request,
+      defaultContentType,
+      imports: imports,
+      mockData: mockData,
+      putInFolder: putInFolder,
+      originalHttpResponse: originalHttpResponse,
+      extrasParameterByDefault: extrasParameterByDefault,
+      dioOptionsParameterByDefault: dioOptionsParameterByDefault,
     );
+    sb.write(data.$1);
+    importStr.writeAll(data.$2);
   }
   sb.write('}\n');
-  return sb.toString();
+  return '${generatedFileComment(markFileAsGenerated: markFileAsGenerated)}${_hideHeaders(restClient, defaultContentType)}$importStr\n$sb';
 }
 
-String _toClientRequest(
+(String, List<String>) _toClientRequest(
   UniversalRequest request,
   String defaultContentType, {
   required bool putInFolder,
@@ -101,26 +101,33 @@ String _toClientRequest(
     sb.write(')');
   }
 
+  var import = <String>[];
+
   if (responseType.contains('List')) {
     final name = responseType.split('<')[1].split('>')[0];
+    final data = _getModelMock(name, imports, putInFolder, mockData);
+    import = data.$2;
     sb
       ..writeln(
         ' async {\n    return List.generate(10, (index) {\n      return $name(',
       )
-      ..writeln(_getModelMock(name, imports, putInFolder, mockData))
+      ..writeln(data.$1)
       ..writeln('  );\n  });\n  }');
   } else {
+    final data = _getModelMock(responseType, imports, putInFolder, mockData);
+    import = data.$2;
+
     sb
       ..writeln(' async {\n    return $responseType(')
-      ..writeln(_getModelMock(responseType, imports, putInFolder, mockData))
+      ..writeln(data.$1)
       ..writeln('    );')
       ..writeln('  }');
   }
 
-  return sb.toString();
+  return (sb.toString(), import);
 }
 
-String _getModelMock(
+(String, List<String>) _getModelMock(
   String type,
   List<String> imports,
   bool putInFolder,
@@ -129,14 +136,22 @@ String _getModelMock(
   final name = type.contains('List') ? type.split('<')[1].split('>')[0] : type;
   var path = imports.where((e) => e.contains(name.toSnake)).firstOrNull;
   if (path == null) {
-    return '';
+    return ('', []);
   }
 
   path =
       'lib/${putInFolder ? 'data' : ''}/${path.replaceAll('../', '').replaceFirst("import '", '').replaceFirst("';", '')}';
 
-  if (!File(path).existsSync()) return '';
+  if (!File(path).existsSync()) return ('', []);
 
+  return parseModel(path, mockData, putInFolder);
+}
+
+(String, List<String>) parseModel(
+  String path,
+  YamlMap mockData,
+  bool putInFolder,
+) {
   final result = parseFile(
     path: path,
     featureSet: FeatureSet.latestLanguageVersion(),
@@ -144,6 +159,8 @@ String _getModelMock(
   final unit = result.unit;
 
   final sb = StringBuffer();
+
+  final imports = <String>[];
 
   for (final declaration in unit.declarations) {
     if (declaration is ClassDeclaration) {
@@ -162,73 +179,94 @@ String _getModelMock(
               defaultValue = param.defaultValue?.toSource();
             }
 
-            final value =
-                _genMockValue(param.toSource(), defaultValue, name, mockData);
+            final value = _genMockValue(
+              param.toSource(),
+              defaultValue,
+              name,
+              mockData,
+              putInFolder,
+            );
 
             if (param.isNamed) {
-              sb.writeln('$name: $value,');
+              sb.writeln('$name: ${value[0]},');
             } else {
-              sb.writeln(value);
+              sb.writeln(value[0]);
             }
+
+            if (value.length > 1) imports.add('${value[1]}');
           }
         }
       }
     }
   }
 
-  return sb.toString();
+  return (sb.toString(), imports);
 }
 
-dynamic _genMockValue(
+List<dynamic> _genMockValue(
   String string,
   String? defaultValue,
   String name,
   YamlMap mockData,
+  bool putInFolder,
 ) {
   final list = string.split(' ');
   final type = list[list.length - 2];
 
   if (mockData.containsKey(name)) {
     final value = mockData[name];
-    if (type == 'String') return '"$value"';
+    if (type == 'String' && !'$value'.contains('faker')) return ['"$value"'];
 
-    return value;
+    return [value];
   }
 
   if (defaultValue != null) {
-    return defaultValue;
+    return [defaultValue];
   }
 
   if (type == 'String') {
-    if (name == 'email') return 'faker.internet.email()';
-    if (name == 'username') return 'faker.internet.userName()';
-    if (name == 'name') return 'faker.person.name()';
-    if (name == 'uuid') return 'faker.guid.guid()';
-    if (name == 'image') return 'faker.image.loremPicsum()';
-    if (name == 'accessToken') return 'faker.jwt.valid()';
-    if (name == 'refreshToken') return 'faker.jwt.valid()';
+    if (name == 'email') return ['faker.internet.email()'];
+    if (name == 'username') return ['faker.internet.userName()'];
+    if (name == 'name') return ['faker.person.name()'];
+    if (name == 'uuid') return ['faker.guid.guid()'];
+    if (name == 'image') return ['faker.image.loremPicsum()'];
+    if (name == 'accessToken') return ['faker.jwt.valid()'];
+    if (name == 'refreshToken') return ['faker.jwt.valid()'];
     if (name == 'phone') {
-      return "faker.randomGenerator.fromPattern(['09#########'])";
+      return ["faker.randomGenerator.fromPattern(['09#########'])"];
     }
     if (name == 'sessionToken') {
-      return 'faker.jwt.valid(expiresIn: DateTime.now().add(Duration(minutes: 30)))';
+      return [
+        'faker.jwt.valid(expiresIn: DateTime.now().add(Duration(minutes: 30)))',
+      ];
     }
     if (name == 'gender') {
-      return "faker.randomGenerator.boolean()?'Male':'Female'";
+      return ["faker.randomGenerator.boolean()?'Male':'Female'"];
     }
 
-    return '""';
+    return ['""'];
   }
 
   if (type == 'int') {
-    return 'faker.randomGenerator.integer(100)';
+    return ['faker.randomGenerator.integer(100)'];
   }
 
   if (type == 'bool') {
-    return 'faker.randomGenerator.boolean()';
+    return ['faker.randomGenerator.boolean()'];
   }
 
-  return '""';
+  if (type.contains('List')) {
+    return ['[]'];
+  }
+
+  final path = 'lib/${putInFolder ? 'data' : ''}/models/${type.toSnake}.dart';
+
+  if (!File(path).existsSync()) return ['""'];
+
+  return [
+    '$type(${parseModel(path, mockData, putInFolder).$1})',
+    "import '${path.replaceFirst('lib', '')}';",
+  ];
 }
 
 String _convertImport(UniversalRestClient restClient) =>
